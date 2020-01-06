f437c5b2ebf7da376da50833cc3d5e6859ccde15
Schlage Michaela <michaela.schlage@evorait.com>
2020-01-06 09:55:25 +0000

    Change entitySet names

--- webapp/block/activities/ActivitiesTableBlockController.controller.js
+++ webapp/block/activities/ActivitiesTableBlockController.controller.js
@@ -1,65 +1,61 @@
 /*global location*/
 sap.ui.define([
-		"com/evorait/evonotify/controller/BaseController",
-		"sap/ui/core/routing/History",
-		"sap/ui/model/json/JSONModel",
-		"com/evorait/evonotify/model/formatter"
-	], function (
-		BaseController,
-		History,
-		JSONModel,
-		formatter
-	) {
-		"use strict";
+	"com/evorait/evonotify/controller/BaseController",
+	"sap/ui/core/routing/History",
+	"sap/ui/model/json/JSONModel",
+	"com/evorait/evonotify/model/formatter"
+], function (
+	BaseController,
+	History,
+	JSONModel,
+	formatter
+) {
+	"use strict";
 
-		return BaseController.extend("com.evorait.evonotify.block.activities.ActivitiesTableBlockController", {
+	return BaseController.extend("com.evorait.evonotify.block.activities.ActivitiesTableBlockController", {
 
-			formatter: formatter,
+		formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
 
-			/**
-			 * Called when the worklist controller is instantiated.
-			 * @public
-			 */
-			onInit : function () {
-				
-			},
-			
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
+		/**
+		 * Called when the worklist controller is instantiated.
+		 * @public
+		 */
+		onInit: function () {
 
-			onPressItem : function (oEvent) {
-				var mParams = {
-					oContext: oEvent.getSource().getBindingContext()
+		},
+
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
+
+		onPressItem: function (oEvent) {
+			var mParams = {
+				oContext: oEvent.getSource().getBindingContext()
+			};
+			this.getOwnerComponent().oAddEntryDialog.open(this.getView(), mParams, "AddEditActivity");
+		},
+
+		onPressAdd: function (oEvent) {
+			var oView = this.getView(),
+				oContext = oView.getBindingContext(),
+				mParams = {
+					sSetPath: "/PMNotificationActivitySet",
+					mKeys: {
+						MaintenanceNotification: oContext.getProperty("MaintenanceNotification"),
+						MaintenanceNotificationItem: oContext.getProperty("MaintenanceNotificationItem")
+					}
 				};
-				this.getOwnerComponent().oAddEntryDialog.open(this.getView(), mParams, "AddEditActivity");
-			},
+			this.getOwnerComponent().oAddEntryDialog.open(oView, mParams, "AddEditActivity");
+		}
 
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
 
-			onPressAdd: function (oEvent) {
-				var oView = this.getView(),
-					oContext = oView.getBindingContext(),
-					mParams = {
-						sSetPath: "/PMNotificationActivities",
-						mKeys: {
-							MaintenanceNotification: oContext.getProperty("MaintenanceNotification"),
-							MaintenanceNotificationItem: oContext.getProperty("MaintenanceNotificationItem")
-						}
-					};
-				this.getOwnerComponent().oAddEntryDialog.open(oView, mParams, "AddEditActivity");
-			}
+	});
 
-
-			/* =========================================================== */
-			/* internal methods                                            */
-			/* =========================================================== */
-			
-
-		});
-
-	}
-);
+});
