db9576cd6f05ff4f4673ef9f8704e3194e7f2b7e
Rahul Inamadar <rahul.inamadar@evorait.com>
2020-05-15 11:58:34 +0530

    EVOSUITE2-109-App-Navigation - Navigation logic is in place

--- webapp/model/models.js
+++ webapp/model/models.js
@@ -36,7 +36,13 @@ sap.ui.define([
 			var oModel = new JSONModel(User);
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
-		}
+		},
+
+        createNavLinksModel : function(NavLinks){
+			var oModel = new JSONModel(NavLinks);
+			oModel.setDefaultBindingMode("TwoWay");
+			return oModel;
+    	}
 
 	};
 });
