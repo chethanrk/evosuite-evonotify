2fc296fb233dc96c5f5d11f3cc7570e3e2cd9ca5
Schlage Michaela <michaela.schlage@evorait.com>
2020-06-04 15:42:28 +0000

    Add dialog template controllers for adding new item

---
+++ webapp/controller/AddEditEntry.controller.js
@@ -0,0 +1,104 @@
+sap.ui.define([
+	"com/evorait/evonotify/controller/DialogFormController",
+	"com/evorait/evonotify/model/formatter"
+], function (DialogFormController, formatter) {
+	"use strict";
+
+	return DialogFormController.extend("com.evorait.evonotify.controller.AddEditEntry", {
+
+		_type: {
+			add: true
+		},
+
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
+
+		/* =========================================================== */
+		/* Events                                                      */
+		/* =========================================================== */
+
+		/**
+		 * @param oEvent
+		 */
+		onChangeSmartField: function (oEvent) {},
+
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
+
+		/**
+		 * Binding has changed in TemplateRenderController
+		 * Set new controller context and path
+		 * and load plant and new operation number when required
+		 * @param sChannel
+		 * @param sEvent
+		 * @param oData
+		 */
+		_changedBinding: function (sChannel, sEvent, oData) {
+			DialogFormController.prototype._changedBinding.apply(this, arguments);
+
+			if (oData && (oData.viewNameId === this._sViewNameId)) {
+				this._getDefaultGlobalParameters();
+				this._oDialog.setContentWidth("100%");
+
+				//prefill planning plant for add and split
+				if (this._type.add) {
+					this._setContextKeys();
+					this._getNextSortNumber(this._setNewSortNumber.bind(this));
+				}
+			}
+		},
+
+		/**
+		 * prefill Plant from header
+		 * prefill WorkOrder from header
+		 * prefill ReservationNumber from header
+		 */
+		_setContextKeys: function () {
+			if (this._oParentObject) {
+				if (this._mParams.mKeys) {
+					Object.keys(this._mParams.mKeys).forEach(function (key) {
+						this.getModel().setProperty(this._sPath + "/" + key, this._mParams.mKeys[key]);
+					}.bind(this));
+				}
+			}
+		},
+
+		/**
+		 * get the next Item sorting number
+		 * and count one upwards
+		 */
+		_getNextSortNumber: function (callbackFn) {
+			var sParentPath = this._oParentContext.getPath();
+
+			this.getModel().read(sParentPath + this._mParams.sNavTo, {
+				sorters: [new sap.ui.model.Sorter(this._mParams.sSortField, "DESCENDING")],
+				success: function (items) {
+					var sortNo = "";
+					if (items.results.length > 0) {
+						sortNo = items.results[0][this._mParams.sSortField];
+						sortNo = parseInt(sortNo) || 0;
+						sortNo = (sortNo + 1);
+						sortNo = formatter.formatSortNumber(sortNo.toString(), 4);
+					} else {
+						sortNo = "0001";
+					}
+					callbackFn(sortNo);
+				}.bind(this),
+				error: function (error) {
+					//do nothing
+				}
+			});
+		},
+
+		/**
+		 * Set sort number in the new context
+		 */
+		_setNewSortNumber: function (sSortNumber) {
+			if (sSortNumber) {
+				this.getModel().setProperty(this._sPath + "/" + this._mParams.sSortField, sSortNumber);
+			}
+		}
+	});
+});
